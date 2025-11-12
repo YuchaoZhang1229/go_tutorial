6ec26dd2551d8f63d9871bad07a5913b0a4a70bb
YuchaoZhang1229 <yuchaozhang1229@gmail.com>
2025-11-12 15:48:00 +0800

    流程控制

---
+++ 01go/05流程控制/main.go
@@ -0,0 +1,90 @@
+package main
+
+import "fmt"
+
+func ifDemo1() {
+	score := 65
+	if score >= 90 {
+		fmt.Println("A")
+	} else if score > 75 {
+		fmt.Println("B")
+	} else {
+		fmt.Println("C")
+	}
+}
+
+func switchDemo1() {
+	finger := 3
+	switch finger {
+	case 1:
+		fmt.Println("大拇指")
+	case 2:
+		fmt.Println("食指")
+	case 3:
+		fmt.Println("中指")
+	case 4:
+		fmt.Println("无名指")
+	case 5:
+		fmt.Println("小拇指")
+	default:
+		fmt.Println("无效的输入！")
+	}
+}
+
+// 一个分支可以有多个值，多个case值中间使用英文逗号分隔。
+func switchDemo2() {
+	switch n := 7; n {
+	case 1, 3, 5, 7, 9:
+		fmt.Println("奇数")
+	case 2, 4, 6, 8:
+		fmt.Println("偶数")
+	default:
+		fmt.Println(n)
+	}
+}
+
+// 分支还可以使用表达式，这时候switch语句后面不需要再跟判断变量。例如：
+func switchDemo3() {
+	age := 30
+	switch {
+	case age < 25:
+		fmt.Println("好好学习吧")
+	case age > 25 && age < 35:
+		fmt.Println("好好工作吧")
+	case age > 60:
+		fmt.Println("好好享受吧")
+	default:
+		fmt.Println("活着真好")
+	}
+}
+
+// fallthrough语法可以执行满足条件的case的下一个case，是为了兼容C语言中的case设计的。
+func switchDemo4() {
+	s := "a"
+	switch {
+	case s == "a":
+		fmt.Println("a")
+		fallthrough
+	case s == "b":
+		fmt.Println("b")
+	case s == "c":
+		fmt.Println("c")
+	default:
+		fmt.Println("...")
+	}
+}
+
+func forDemo() {
+	for i := 0; i < 10; i++ {
+		fmt.Print(i)
+	}
+}
+
+func main() {
+	ifDemo1()
+	switchDemo1()
+	switchDemo2()
+	switchDemo3()
+	switchDemo4()
+	forDemo()
+}
